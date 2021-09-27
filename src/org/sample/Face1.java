8bd40ae64698f7cad987467280868a83426b297b
Lakshminarayanan12 <alnsaravanaraj@yahoo.com>
2021-09-27 15:17:03 +0530

    Folder

---
+++ src/org/sample/Face1.java
@@ -0,0 +1,8 @@
+package org.sample;
+
+public class Face1 {
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ src/org/sample1/Face2.java
@@ -0,0 +1,8 @@
+package org.sample1;
+
+public class Face2 {
+	public static void main(String[] args) {
+		
+	}
+
+}
